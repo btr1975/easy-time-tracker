e1cf7d4473f8c6e24e9c46516b78f7939c34b96d
Ben <btr1975@gmail.com>
2022-02-11 12:51:49 -0600

    Update to setup.py for pandas versions

--- setup.py
+++ setup.py
@@ -18,8 +18,9 @@ package_data = {}
 
 install_requires = [
     "pydantic~=1.9.0",
-    "pandas~=1.1.5; python_version <= '3.6'",
-    "pandas~=1.4.0; python_version >= '3.7'",
+    "pandas~=1.1.5; python_version == '3.6'",
+    "pandas~=1.3.5; python_version == '3.7'",
+    "pandas~=1.4.0; python_version >= '3.8'",
     "openpyxl~=3.0.9"
 ]
 
@@ -68,5 +69,6 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 )
